6c7e6dd998b1734e44247938f68c2fe793547b58
Rene Rednoss <rednoss@e-spirit.com>
2018-07-03 08:55:44 +0200

    Update for release 1.2.1

--- src/main/java/com/espirit/moddev/basicworkflows/release/WfReleaseExecutable.java
+++ src/main/java/com/espirit/moddev/basicworkflows/release/WfReleaseExecutable.java
@@ -31,6 +31,8 @@ import de.espirit.firstspirit.access.store.IDProvider;
 import de.espirit.firstspirit.access.store.Store;
 import de.espirit.firstspirit.access.store.contentstore.ContentWorkflowable;
 import de.espirit.firstspirit.access.store.sitestore.PageRef;
+import de.espirit.firstspirit.access.store.sitestore.SiteStoreFolder;
+import de.espirit.firstspirit.access.store.sitestore.StartNode;
 import de.espirit.firstspirit.access.store.templatestore.WorkflowScriptContext;
 
 import java.util.ArrayList;
@@ -52,7 +54,6 @@ public class WfReleaseExecutable extends AbstractWorkflowExecutable {
      */
     public static final Class<?> LOGGER = WfReleaseExecutable.class;
 
-
     @Override
     @SuppressWarnings("unchecked")
     public Object execute(final Map<String, Object> params) {
@@ -70,14 +71,14 @@ public class WfReleaseExecutable extends AbstractWorkflowExecutable {
         final List<Object> releaseObjects = new ArrayList<>();
 
         final List<IDProvider> childrenList = new ArrayList<>();
-		final Map<Long, Store.Type> childrenIdMap = WorkflowSessionHelper.readObjectFromSession(workflowScriptContext, WorkflowConstants.WF_RECURSIVE_CHILDREN);
+        final Map<Long, Store.Type> childrenIdMap = WorkflowSessionHelper.readObjectFromSession(workflowScriptContext, WorkflowConstants.WF_RECURSIVE_CHILDREN);
         childrenList.addAll(loadChildrenList(workflowScriptContext, childrenIdMap));
 
         final IDProvider releaseElement = workflowScriptContext.getElement();
 
         // check test case or skip if wfDoFail is set
         if (isNotFailed(workflowScriptContext)) {
-			final Object releasePageRefElements = WorkflowSessionHelper.readObjectFromSession(workflowScriptContext, WorkflowConstants.RELEASE_PAGEREF_ELEMENTS);
+            final Object releasePageRefElements = WorkflowSessionHelper.readObjectFromSession(workflowScriptContext, WorkflowConstants.RELEASE_PAGEREF_ELEMENTS);
             List<String> releasePageRefUids = null;
             if (releasePageRefElements != null && releasePageRefElements instanceof List) {
                 releasePageRefUids = (List<String>) releasePageRefElements;
@@ -128,11 +129,26 @@ public class WfReleaseExecutable extends AbstractWorkflowExecutable {
                 }
 
                 addChildrenToReleaseObjects(workflowObject, releaseWithMedia, releaseRecursively, releaseObjects,
-                                            releaseElementsWithPossibleChildren);
+                    releaseElementsWithPossibleChildren);
 
                 // do release
                 final ReleaseObject releaseObject = new ReleaseObject(workflowScriptContext, releaseObjects);
                 releaseStatus = releaseObject.release(false, releaseRecursively);
+
+                // check for never released start nodes in one or more parent folders
+                if (releaseElement.getStore().getType() == Store.Type.SITESTORE) {
+                    List<StartNode> neverReleasedStartNodes = getNeverReleasedStartNodes(releaseElement);
+                    if (neverReleasedStartNodes != null && !neverReleasedStartNodes.isEmpty()) {
+                        for (StartNode startNode : neverReleasedStartNodes) {
+                            Logging.logInfo("IDProvider Element with uid '" + startNode.getUid() + "' and id '" + startNode.getId() + "' has never been released.", LOGGER);
+                        }
+                        final String suppressDialog = (String) workflowScriptContext.getSession().get("wfSuppressDialog");
+                        if (!WorkflowConstants.TRUE.equals(suppressDialog)) {
+                            showDialog(workflowScriptContext, bundle.getString(WorkflowConstants.ERROR_MSG),
+                                       bundle.getString("neverReleasedStartNodes"));
+                        }
+                    }
+                }
             }
         } else {
             releaseStatus = false;
@@ -167,7 +183,7 @@ public class WfReleaseExecutable extends AbstractWorkflowExecutable {
     }
 
     private static void addChildrenToReleaseObjects(final WorkflowObject workflowObject, final boolean releaseWithMedia, final boolean releaseRecursively,
-                                                    final List<Object> releaseObjects, final List<IDProvider> releaseElementsWithPossibleChildren) {
+        final List<Object> releaseObjects, final List<IDProvider> releaseElementsWithPossibleChildren) {
         for (final IDProvider storeElement : releaseElementsWithPossibleChildren) {
             // create workflowObject with current releasable storeElement
             workflowObject.setRecursively(releaseRecursively);
@@ -182,12 +198,12 @@ public class WfReleaseExecutable extends AbstractWorkflowExecutable {
             for (final Object refObject : refObjects) {
                 if (refObject instanceof IDProvider) {
                     Logging.logInfo("IDProvider referenced Element with Id '" + ((IDProvider) refObject).getId() + "' added to release list",
-                            LOGGER);
+                        LOGGER);
                 } else if (refObject instanceof ReferenceEntry) {
                     final IDProvider referencedElement = ((ReferenceEntry) refObject).getReferencedElement();
                     if (referencedElement != null) {
                         Logging.logInfo("IDProvider referenced Element with Id '" + referencedElement.getId()
-                                        + "' added to release list", LOGGER);
+                            + "' added to release list", LOGGER);
                     } else {
                         Logging.logInfo("ReferenceEntry element is null. Broken Reference?", LOGGER);
                     }
@@ -195,7 +211,6 @@ public class WfReleaseExecutable extends AbstractWorkflowExecutable {
                     Logging.logInfo("Element of class '" + refObject.getClass().toString() + "' can't be fetched", LOGGER);
                 }
             }
-
 
             if (storeElement instanceof PageRef && (((PageRef) storeElement).getPage()).getReleaseStatus() != IDProvider.RELEASED) {
                 // if object is pageref, add page to release list if unreleased
@@ -207,6 +222,31 @@ public class WfReleaseExecutable extends AbstractWorkflowExecutable {
                 releaseObjects.add(storeElement);
             }
         }
+    }
+
+    /**
+     * Get never released start nodes for each parent sitestore folder.
+     *
+     * @param storeElement the store element the workflow started on
+     * @return list of never released start nodes
+     */
+    private static List<StartNode> getNeverReleasedStartNodes(final IDProvider storeElement) {
+        List<StartNode> neverReleasedStartNodes = new ArrayList<>();
+
+        final IDProvider parentElement = storeElement.getParent();
+        if (parentElement != null && !parentElement.getUid().equals("root")) {
+            neverReleasedStartNodes = getNeverReleasedStartNodes(parentElement);
+        }
+
+        if (storeElement instanceof SiteStoreFolder) {
+            final SiteStoreFolder storeElementFolder = (SiteStoreFolder) storeElement;
+            final StartNode startNode = storeElementFolder.getStartNode();
+            if (startNode != null && !startNode.isInReleaseStore()) {
+                neverReleasedStartNodes.add(startNode);
+            }
+        }
+
+        return neverReleasedStartNodes;
     }
 
 }
